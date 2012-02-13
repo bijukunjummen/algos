90c2fa2298aa7efe9ee045a7fa46f59f51c8c2ca
Biju Kunjummen <biju.kunjummen@gmail.com>
2012-02-13 00:50:15 -0500

    Implemented GCD

---
+++ src/test/java/org/bk/algo001/gcd/GcdTests.java
@@ -0,0 +1,29 @@
+package org.bk.algo001.gcd;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+public class GcdTests {
+	
+	@Test
+	public void testGcdOf4And6Is2(){
+		assertThat(Gcd.of(4, 6), is(2));
+	}
+
+	@Test
+	public void testGcdOf4And4Is4(){
+		assertThat(Gcd.of(4, 4), is(4));
+	}
+
+	@Test
+	public void testGcdOf17And23Is1(){
+		assertThat(Gcd.of(17, 23), is(1));
+	}
+
+	@Test
+	public void testGcdOf27And18Is9(){
+		assertThat(Gcd.of(27, 18), is(9));
+	}
+}
