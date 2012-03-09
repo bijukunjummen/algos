43abce929419f6528212b70842394f213d43c3f9
Biju Kunjummen <biju.kunjummen@gmail.com>
2012-03-08 19:36:13 -0500

    Added Recursive Binary Search

--- src/main/java/org/bk/algo/core/BinarySearch.java
+++ src/main/java/org/bk/algo/core/BinarySearch.java
@@ -7,18 +7,35 @@ public class BinarySearch {
 		
 		while (lo<=hi){
 			int mid = lo + (hi - lo)/2;
-
-			if (arr[mid].compareTo(anItem)>0){
+			int  compareVal = anItem.compareTo(arr[mid]);
+			if (compareVal < 0){
 				hi = mid - 1;
-			}else if (arr[mid].compareTo(anItem)<0){
+			}else if (compareVal > 0){
 				lo = mid + 1;
 			}else{
 				return mid;
 			}
-			
 		}
-		
 		return -1;
+	}
+	
+	public static<T extends Comparable<? super T>> int recSearch(T[] arr, T anItem){
+	    return recSearchHelper(arr, 0, arr.length-1, anItem);
+	}
+	
+	private static<T extends Comparable<? super T>> int recSearchHelper(T[] arr, int lo, int hi, T anItem){
+	    if (lo>hi) return -1;
+
+	    int mid = lo + (hi-lo)/2;	        
+	    int compareVal = anItem.compareTo(arr[mid]);
+	    
+	    if (compareVal==0) 
+	        return mid;
+	    else if (compareVal<0){
+	        return recSearchHelper(arr, lo, mid-1, anItem);
+	    }else{
+	        return recSearchHelper(arr, mid+1, hi, anItem);
+	    }
 	}
 
 }
--- src/test/java/org/bk/algo/core/BinarySearchTest.java
+++ src/test/java/org/bk/algo/core/BinarySearchTest.java
@@ -7,18 +7,33 @@ import org.junit.Test;
 
 public class BinarySearchTest {
 
-	@Test
-	public void testSearchWithASmallIntegerArray(){
-		Integer[] anArr = {1,2,3,5,9,10,15,2000};
-		assertThat(BinarySearch.search(anArr, 9), is(4));
-		assertThat(BinarySearch.search(anArr, 1), is(0));
-		assertThat(BinarySearch.search(anArr, 2), is(1));
-		assertThat(BinarySearch.search(anArr, 3), is(2));
-		assertThat(BinarySearch.search(anArr, 5), is(3));
-		assertThat(BinarySearch.search(anArr, 9), is(4));
-		assertThat(BinarySearch.search(anArr, 10), is(5));
-		assertThat(BinarySearch.search(anArr, 15), is(6));
-		assertThat(BinarySearch.search(anArr, 20), is(-1));
-		
-	}
+    @Test
+    public void testSearchWithASmallIntegerArray() {
+        Integer[] anArr = { 1, 2, 3, 5, 9, 10, 15, 2000 };
+        assertThat(BinarySearch.search(anArr, 9), is(4));
+        assertThat(BinarySearch.search(anArr, 1), is(0));
+        assertThat(BinarySearch.search(anArr, 2), is(1));
+        assertThat(BinarySearch.search(anArr, 3), is(2));
+        assertThat(BinarySearch.search(anArr, 5), is(3));
+        assertThat(BinarySearch.search(anArr, 9), is(4));
+        assertThat(BinarySearch.search(anArr, 10), is(5));
+        assertThat(BinarySearch.search(anArr, 15), is(6));
+        assertThat(BinarySearch.search(anArr, 20), is(-1));
+
+    }
+
+    @Test
+    public void testSearchWithASmallIntegerArrayAndRecursiveSearch() {
+        Integer[] anArr = { 1, 2, 3, 5, 9, 10, 15, 2000 };
+        assertThat(BinarySearch.recSearch(anArr, 9), is(4));
+        assertThat(BinarySearch.recSearch(anArr, 1), is(0));
+        assertThat(BinarySearch.recSearch(anArr, 2), is(1));
+        assertThat(BinarySearch.recSearch(anArr, 3), is(2));
+        assertThat(BinarySearch.recSearch(anArr, 5), is(3));
+        assertThat(BinarySearch.recSearch(anArr, 9), is(4));
+        assertThat(BinarySearch.recSearch(anArr, 10), is(5));
+        assertThat(BinarySearch.recSearch(anArr, 15), is(6));
+        assertThat(BinarySearch.recSearch(anArr, 20), is(-1));
+
+    }
 }
