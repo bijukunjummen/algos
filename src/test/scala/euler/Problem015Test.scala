c6346cfe04b2f8408c87ae25ccbcc9be7c1582c4
Biju Kunjummen <Biju.Kunjummen@gmail.com>
2014-05-11 09:14:16 -0400

    Problem 15 Euler

---
+++ src/test/scala/euler/Problem015Test.scala
@@ -0,0 +1,26 @@
+package euler
+
+import org.scalatest.FunSuite
+import scala.collection.immutable.Stream.consWrapper
+import scala.math.BigInt
+
+class Problem015Test extends FunSuite {
+  
+  test("Lattice paths") {
+	  assert(choose(40, 20) === BigInt("137846528820"))
+  }
+  
+  def choose(n: Int, m: Int): BigInt = {
+	fact(n, m)/fact(m, 1)
+  }
+ 
+  
+  def fact(n1: Int, n2: Int ): BigInt = {
+    def fact(res: BigInt, m1: Int, m2: Int): BigInt = {
+      if (m1 <= m2) res
+      else fact(res*m1, m1 - 1, m2)
+    }
+    fact(1, n1, n2)
+  }
+  
+}
---
+++ src/test/scala/euler/notworking/Problem015TestAltNotWorking.scala
@@ -0,0 +1,59 @@
+package euler.notworking
+
+import org.scalatest.FunSuite
+import scala.collection.immutable.Stream.consWrapper
+
+class Problem015TestNotWorking extends FunSuite with Solver {
+  val startPos = Position(0, 0)
+  val goalPos = Position(20, 20)
+  val grid = Grid(20, 20)
+
+  test("Lattice paths") {
+    pathsToGoal
+  }
+}
+
+trait Lattice {
+  sealed abstract class Move
+  case object Right extends Move
+  case object Down extends Move
+
+  case class Position(r: Int, c: Int) {
+    def down = copy(r + 1, c)
+    def right = copy(r, c + 1)
+    def nextMoves: List[(Position, Move)]  = {
+      (down, Down) :: (right, Right) :: Nil
+    }
+    def legalMoves = nextMoves filter { case (p,m) => p.isLegal }
+    def isLegal = (r <= grid.nr && c <= grid.nc)
+  }
+
+  case class Grid(nr: Int, nc: Int)
+  val startPos: Position
+  val goalPos: Position
+  val grid: Grid
+}
+
+trait Solver extends Lattice {
+  def done(p: Position) = (p == goalPos)
+  def neighborsWithHistory(p: Position, history: List[Move]): Stream[(Position, List[Move])] = {
+    p.legalMoves.toStream.map {case (p, m) => (p, m :: history)}
+  }
+  
+  def from(initial: Stream[(Position, List[Move])]): Stream[(Position, List[Move])] = {
+    if (initial.isEmpty) Stream.empty
+    else {
+      val more = for {
+        (p, h) <- initial
+        next <- neighborsWithHistory(p, h)
+      } yield next
+      initial #::: from(more)
+    }
+  }
+  
+  lazy val pathsFromStart = from(List((startPos, List())).toStream)
+  lazy val pathsToGoal = for {
+    (p, h) <- pathsFromStart
+    if (p == goalPos)
+  } yield (p, h)
+}
