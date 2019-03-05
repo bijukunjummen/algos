c10b8db8724c7caacc592d75afecdceb08e19eca
Biju Kunjummen <biju.kunjummen@gmail.com>
2019-03-04 21:27:46 -0800

    Polish

--- src/test/kotlin/pour/PouringTest.kt
+++ src/test/kotlin/pour/PouringTest.kt
@@ -30,7 +30,6 @@ class PouringTest {
                         val movesAndStateForPath = getMovesAndStateForPath(path, initialState)
                         if (!movesAndStateForPath.isEmpty) {
                             val moveAndState = movesAndStateForPath.last()
-//                        movesAndStateForPath.forEach { moveAndState ->
                             val move = moveAndState._1
                             val states = moveAndState._2
                             val from = states._1
@@ -38,41 +37,11 @@ class PouringTest {
                             println("\"$from\" -> \"$to\" [label=\"$move\"]")
 
                         }
-//                        }
                     }
                 }
-//        pouring.pathSets.take(3).toList()[1].forEach {path ->
-//            val movesAndStateForPath = getMovesAndStateForPath(path, initialState)
-//
-//            movesAndStateForPath.forEach{moveAndState ->
-//                val move = moveAndState._1
-//                val states = moveAndState._2
-//                val from = states._1
-//                val to = states._2
-//                println("\"$from\" -> \"$to\" [label=\"$move\"]")
-//            }
-//        }
         println("}")
-//        pouring.solution(list(Cup(0, 4), Cup(6, 9)))
-//                .take(1).forEach { path ->
-//                    val res = path.history.reverse().foldLeft(Tuple.of(list<State>(), initialState)) { tup, move ->
-//                        val newState = move.change(tup._2)
-//                        val updatedList = tup._1.prepend(newState)
-//                        Tuple.of(updatedList, newState)
-//                    }
-//                    path.history.reverse().zip(res._1.reverse()).forEach { tup ->
-//                        println("Move: ${tup._1} --> ${tup._2}")
-//                    }
-//                }
-
     }
 
-    //    fun getGraph(path: Path, initialState: State): List<Tuple3<Move, State, State>> {
-//        val res = path.history.reverse().foldLeft(List.empty<Tuple3<Move, State, State>>()) { l, m ->
-//            l
-//
-//        }
-//    }
     fun getMovesAndStateForPath(path: Path, initialState: State): List<Tuple2<Move, Tuple2<State, State>>> {
         val res = path.history.reverse().foldLeft(Tuple.of(List.empty<Tuple2<State, State>>(), initialState)) { tup, move ->
             val oldState = tup._2
